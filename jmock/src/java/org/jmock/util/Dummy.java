ac1675a27322b5b8a201d8d65e7c64e3b5962c5c
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2003-12-08 21:37:10 +0000

    Added Dummy class that wraps the use of mocks to act as dummy implementations of interfaces

---
+++ jmock/src/java/org/jmock/util/Dummy.java
@@ -0,0 +1,43 @@
+/* Copyright (c) 2000-2003, jMock.org. See LICENSE.txt */
+package org.jmock.util;
+
+import org.jmock.dynamic.CoreMock;
+import org.jmock.dynamic.Invocation;
+import org.jmock.dynamic.InvocationMocker;
+import org.jmock.dynamic.LIFOInvocationDispatcher;
+import org.jmock.dynamic.matcher.StatelessInvocationMatcher;
+import org.jmock.dynamic.stub.CustomStub;
+
+public class Dummy {
+	/** This class cannot be instantiated. */
+	private Dummy() {
+	}
+
+	public static Object newDummy(Class interfaceClass) {
+		return newDummy( interfaceClass, "dummy"+CoreMock.className(interfaceClass) );
+	}
+
+	public static Object newDummy( final Class interfaceClass, final String name ) {
+		CoreMock mock = new CoreMock( interfaceClass, name, new LIFOInvocationDispatcher() );
+        InvocationMocker mocker = new InvocationMocker();
+        
+        mocker.addMatcher( new StatelessInvocationMatcher() {
+            public boolean matches( Invocation invocation ) {
+            	return invocation.getDeclaringClass() == interfaceClass;
+            }
+            public StringBuffer writeTo(StringBuffer buf) {
+            	return buf.append("anu method declared in " + interfaceClass);
+            }
+        } );
+        mocker.setStub( new CustomStub("dummy method") {
+        	public Object invoke( Invocation invocation ) throws Throwable {
+                throw new NotImplementedException( 
+                    invocation.getMethodName() + "called on " + name );
+            }
+        } );
+        
+        mock.add(mocker);
+        
+        return mock.proxy();
+	}
+}
---
+++ jmock/src/java/test/jmock/util/DummyTest.java
@@ -0,0 +1,61 @@
+/* Copyright (c) 2000-2003, jMock.org. See LICENSE.txt */
+package test.jmock.util;
+
+import org.jmock.util.Dummy;
+import org.jmock.util.NotImplementedException;
+
+
+import junit.framework.TestCase;
+
+
+public class DummyTest extends TestCase {
+    private static final String DUMMY_NAME = "DUMMY NAME";
+    
+	interface ExampleInterface {
+    	void method1();
+    }
+    
+    public void testImplementsAGivenInterface() {
+    	Object dummy = Dummy.newDummy(ExampleInterface.class);
+        
+        assertTrue( "should be instanceof ExampleInterface",
+                    dummy instanceof ExampleInterface );
+    }
+    
+    public void testCanBeGivenAnExplicitNameThatIsReturnedByToString() {
+    	Object dummy = Dummy.newDummy(ExampleInterface.class,DUMMY_NAME);
+        
+        assertEquals( "should return name from toString",
+                      DUMMY_NAME, dummy.toString() );
+    }
+    
+    public void testGetsAUsefulDefaultName() {
+        Object dummy = Dummy.newDummy(ExampleInterface.class);
+        
+        assertEquals( "should return name from toString",
+                      "dummyDummyTest$ExampleInterface", dummy.toString() );
+    }
+    
+    public void testImplementsEqualsByComparingObjectReferences() {
+    	Object dummy = Dummy.newDummy(ExampleInterface.class);
+        
+        assertTrue( "should be equal to itself", dummy.equals(dummy) );
+        assertFalse( "should not be equal to another object", dummy.equals(new Object()) );
+    }
+    
+    public void testThrowsNotImplementedExceptionFromEveryMethod() {
+        ExampleInterface dummy = 
+            (ExampleInterface)Dummy.newDummy(ExampleInterface.class,DUMMY_NAME);
+        
+        try {
+    		dummy.method1();
+            fail("NotImplementedException expected from method1");
+        }
+        catch( NotImplementedException ex ) {
+        	assertTrue( "error message should contain name of dummy object",
+                        ex.getMessage().indexOf(DUMMY_NAME) >= 0 );
+            assertTrue( "error message should contain name of method",
+                        ex.getMessage().indexOf("method1") >= 0 );
+        }
+    }
+}
