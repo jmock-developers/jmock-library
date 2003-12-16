07b53236892fa47515243604a6a6744c130acb91
smgf <smgf@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2003-12-16 16:54:24 +0000

    [sf][np] Extracted AnyArgumentsMatcher and NoArgumentsMatcher from C class

---
+++ src/java/org/jmock/dynamic/matcher/AnyArgumentsMatcher.java
@@ -0,0 +1,16 @@
+package org.jmock.dynamic.matcher;
+
+import org.jmock.dynamic.Invocation;
+
+
+public class AnyArgumentsMatcher extends StatelessInvocationMatcher {
+    
+    public static final AnyArgumentsMatcher INSTANCE = new AnyArgumentsMatcher();
+    
+    public boolean matches(Invocation invocation) {
+        return true;
+    }
+    public StringBuffer writeTo(StringBuffer buffer) {
+        return buffer.append("Any arguments");
+    }
+}
---
+++ src/java/org/jmock/dynamic/matcher/NoArgumentsMatcher.java
@@ -0,0 +1,15 @@
+package org.jmock.dynamic.matcher;
+
+import org.jmock.dynamic.Invocation;
+
+
+public class NoArgumentsMatcher extends StatelessInvocationMatcher {
+    public static final NoArgumentsMatcher INSTANCE = new NoArgumentsMatcher();
+    
+    public boolean matches(Invocation invocation) {
+        return invocation.getParameterValues().isEmpty();
+    }
+    public StringBuffer writeTo(StringBuffer buffer) {
+        return buffer.append("No arguments");
+    }
+}
