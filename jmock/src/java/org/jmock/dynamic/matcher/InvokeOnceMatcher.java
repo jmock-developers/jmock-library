4ae7dcec91bb3d93be81171534d10e301304ed61
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2003-12-17 19:07:39 +0000

    Refactored to extend InvokedRecorder

--- jmock/src/java/org/jmock/dynamic/matcher/InvokeOnceMatcher.java
+++ jmock/src/java/org/jmock/dynamic/matcher/InvokeOnceMatcher.java
@@ -1,26 +1,20 @@
 /* Copyright (c) 2000-2003, jMock.org. See LICENSE.txt */
 package org.jmock.dynamic.matcher;
 
-import junit.framework.Assert;
 import org.jmock.dynamic.Invocation;
-import org.jmock.dynamic.InvocationMatcher;
 
-public class InvokeOnceMatcher implements InvocationMatcher {
-    private boolean wasInvoked = false;
-
+public class InvokeOnceMatcher
+	extends InvokedRecorder
+{
     public boolean matches(Invocation invocation) {
-        return !wasInvoked;
+        return !hasBeenInvoked();
     }
-
-    public void invoked(Invocation invocation) {
-        wasInvoked = true;
-    }
-
+    
     public void verify() {
-        Assert.assertTrue("Should have been called", wasInvoked);
+    	verifyHasBeenInvoked();
     }
-
+    
     public StringBuffer writeTo(StringBuffer buffer) {
-        return buffer.append("Call once");
+        return buffer.append("called once");
     }
 }
