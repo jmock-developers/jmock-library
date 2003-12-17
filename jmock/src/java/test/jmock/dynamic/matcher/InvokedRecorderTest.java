5e631bfd8cd4ae097e44fe32c35fa3c99ea8c2c3
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2003-12-17 18:57:20 +0000

    Added expectAfter method to ExpectationBuilder interface Created InvokedRecorder class Created mock for ExpectationBuilder interface

---
+++ jmock/src/java/test/jmock/dynamic/matcher/InvokedRecorderTest.java
@@ -0,0 +1,56 @@
+/* Copyright (c) 2000-2003, jMock.org. See LICENSE.txt */
+package test.jmock.dynamic.matcher;
+
+import junit.framework.TestCase;
+
+import org.jmock.dynamic.Invocation;
+import org.jmock.dynamic.matcher.InvokedRecorder;
+import org.jmock.expectation.AssertMo;
+
+public class InvokedRecorderTest extends TestCase {
+    private Invocation emptyInvocation =
+        new Invocation(Void.class, "example", new Class[0], Void.class, new Object[0]);
+    private InvokedRecorder recorder = new InvokedRecorder();
+    
+    public void testAlwaysMatches() {
+        assertTrue( "should match before invocation", 
+                    recorder.matches(emptyInvocation));
+        
+        recorder.invoked(emptyInvocation);
+        
+        assertTrue( "should still match after invocation", 
+                    recorder.matches(emptyInvocation));
+    }
+    
+    public void testAlwaysVerifies() {
+    	recorder.verify();
+        recorder.invoked(emptyInvocation);
+        recorder.verify();
+    }
+    
+    public void testReportsIfHasBeenInvoked() {
+    	assertFalse( "should not have been invoked", recorder.hasBeenInvoked() );
+        recorder.invoked(emptyInvocation);
+        assertTrue( "should have been invoked", recorder.hasBeenInvoked () );
+    }
+    
+    public void testCanExplicitlyVerifyThatItHasBeenInvoked() {
+    	AssertMo.assertFails("verifyHasBeenInvoked should fail", new Runnable() {
+    		public void run() {
+    			recorder.verifyHasBeenInvoked();
+    		}
+    	} );
+        recorder.invoked(emptyInvocation);
+        recorder.verifyHasBeenInvoked();
+    }
+    
+    public void testCanExplicitlyVerifyThatItHasNotBeenInvoked() {
+    	recorder.verifyHasNotBeenInvoked();
+    	recorder.invoked(emptyInvocation);
+    	AssertMo.assertFails("verifyHasNotBeenInvoked should fail", new Runnable() {
+    		public void run() {
+    			recorder.verifyHasNotBeenInvoked();
+    		}
+    	} );
+    }
+}
