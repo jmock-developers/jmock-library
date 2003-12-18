587243bea132ecc817bb2bda309c022d4eaf5959
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2003-12-18 20:44:08 +0000

    Some tasks done, some new tasks

--- src/java/test/jmock/ToDo.java
+++ src/java/test/jmock/ToDo.java
@@ -3,26 +3,31 @@ package test.jmock;
 public class ToDo {
 
     //TODO JavaDoc all classes in root org.jmock package.
-    //TODO Find cool new name for "builder" API
+    //TODO Find cool new name for "builder" API and rename packages
     //TODO Clean up org.jmock.expecation package.
     //TODO Make jmock.expectation package use Constraints instead of calling equals on expected value
     //TODO Better tests for error messages
-    //TODO Implement matchers for call ordering.
+    //DONE Implement matchers for call ordering.
+	//TODO Add method to builder API for ordering calls across mocks
+	//TODO Implement API for call ordering in dynamock API
     //TODO REMOVE ALL TO-DO COMMENTS
-    //Done Extract NoArgs and AnyArgs matcher classes from C
-    //Done Stop core packages relying on C
-    //TODO Put low-level hook methods into builder interfaces
+    //DONE Extract NoArgs and AnyArgs matcher classes from C
+    //DONE Stop core packages relying on C
+    //TODO Put low-level hook methods into builder interfaces (just need to be able to add Invokables to Mock)
     //TODO Remove getters from Invocation ? 
         
-    //    (sensible defaults reduce test brittleness)
-    //TODO: Sensible default: If no expectation/match setup, invocation should produce no-op instead of fail.
-    // TODO: ExpectNotCalled (or equivalent) makes tests more expressive.
+    // Sensible defaults to reduce test brittleness:
+    //TODO Sensible default: If no expectation/match setup, invocation should produce no-op instead of fail.
+    //TODO ExpectNotCalled (or equivalent) makes tests more expressive.
     
-    //        *** Suggested improvements ***
-    // (sensible defaults reduce test brittleness)
+    // Suggested improvements:
+    // Sensible defaults reduce test brittleness:
     // - Sensible default: When no args specified in Mock, use ANY-args instead of NO-args.
     //  -- I disagree: if you allow any arguments, putting C.ANY_ARGS shows what you are doing and isn't particularly verbose
+	//  -- But, this is the behaviour of the builder API
+	//  -- But but, it is a very subtle change from com.mockobjects API
     // - Extract interface from Invocation.
     //  -- WHY? It's a pure value object with no behaviour.
+	//  -- But, could add useful behaviour to it
                   
 }
