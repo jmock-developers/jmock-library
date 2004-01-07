ea20791153327d53863643ba8464ed403b4d3b40
smgf <smgf@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2004-01-07 12:44:57 +0000

    [sf] added failing test to catch problems with order of return values

---
+++ src/java/test/jmock/dynamock/AdditionalReturnMockTest.java
@@ -0,0 +1,26 @@
+/* Copyright (c) 2000-2003, jMock.org. See LICENSE.txt */
+package test.jmock.dynamock;
+
+import junit.framework.TestCase;
+
+import org.jmock.dynamock.C;
+import org.jmock.dynamock.Mock;
+
+public class AdditionalReturnMockTest extends TestCase {
+	public interface WithReturnValue {
+		Object withNoParams();
+    }
+    
+    public void testReturnsMultipleValuesInOrderSpecified() {
+    	Mock mock = new Mock(WithReturnValue.class);
+        
+        mock.expectAndReturn("withNoParams", C.NO_ARGS, "one");
+        mock.expectAndReturn("withNoParams", C.NO_ARGS, "two");
+        
+        WithReturnValue returner = (WithReturnValue)mock.proxy();
+        assertEquals("first call", "one", returner.withNoParams());
+        assertEquals("second call", "two", returner.withNoParams());
+        
+        mock.verify();
+    }
+}
