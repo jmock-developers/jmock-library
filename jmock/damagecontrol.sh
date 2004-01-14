bdb61138f8da9f60bb82923620e15f924de01a55
smgf <smgf@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2004-01-14 21:40:55 +0000

    Commented the script

--- jmock/damagecontrol.sh
+++ jmock/damagecontrol.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+# Damage control build script for jMock.
 
 WEBSITE=${WEBSITE:-dcontrol@www.codehaus.org:/www/jmock.codehaus.org/}
 
 cd website
 ruby ./skinner.rb
 scp -r output/* $WEBSITE
+
