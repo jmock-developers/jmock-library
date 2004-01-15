7d2d3478753c61ba060a571cd3475933a0481917
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2004-01-15 14:38:30 +0000

    *** empty log message ***

--- jmock/damagecontrol.sh
+++ jmock/damagecontrol.sh
@@ -7,13 +7,26 @@ WEBSITE=${WEBSITE:-dcontrol@www.codehaus.org:/www/jmock.codehaus.org/}
 WEBDIR=website/output
 JAVADOCDIR=$WEBDIR/docs/javadoc
 
+# Create clean output directory
+rm -r $WEBDIR
+mkdir $WEBDIR
+
+
+# Generate the skinned and styled site content
 (cd website; ruby ./skinner.rb)
+
+
+# Generate project reports
+
+# Javadoc
 $JAVA_HOME/bin/javadoc \
 	-windowtitle 'jMock API Documentation' \
 	-d $JAVADOCDIR \
-	-link http://www.junit.org/junit/javadoc/3.8.1/ \
-	-link http://java.sun.com/j2se/1.4.2/docs/api/ \
+	-linkoffline http://www.junit.org/junit/javadoc/3.8.1/index.htm \
+	-link http://java.sun.com/j2se/1.4.2/docs/api/index.html \
 	-sourcepath $SRCDIR \
 	-subpackages org.jmock
 
+
+# Copy content to web server
 scp -r $WEBDIR/* $WEBSITE
