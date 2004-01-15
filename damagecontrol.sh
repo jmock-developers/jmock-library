4d5a6a458effe35e95740300ecd6888d6e44dccc
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2004-01-15 14:09:25 +0000

    Link jmock javadoc to junit and java documentation

--- damagecontrol.sh
+++ damagecontrol.sh
@@ -11,6 +11,8 @@ JAVADOCDIR=$WEBDIR/docs/javadoc
 $JAVA_HOME/bin/javadoc \
 	-windowtitle 'jMock API Documentation' \
 	-d $JAVADOCDIR \
+	-link http://www.junit.org/junit/javadoc/3.8.1/ \
+	-link http://java.sun.com/j2se/1.4.2/docs/api/ \
 	-sourcepath $SRCDIR \
 	-subpackages org.jmock
 
