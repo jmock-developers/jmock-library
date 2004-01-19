5f4abc1f05b808972f16716867627791e6c6cbda
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2004-01-19 20:27:00 +0000

    Made acceptance tests for error messages a bit more flexible Cleaned up description text for IsEqual constraints

--- jmock/src/acceptance-tests/atest/jmock/builder/Mock_ErrorMessage_IntegrationTest.java
+++ jmock/src/acceptance-tests/atest/jmock/builder/Mock_ErrorMessage_IntegrationTest.java
@@ -5,50 +5,89 @@ package atest.jmock.builder;
 
 import org.jmock.builder.Mock;
 import org.jmock.builder.MockObjectTestCase;
+import org.jmock.dynamic.DynamicMockError;
 
 public class Mock_ErrorMessage_IntegrationTest extends MockObjectTestCase {
 
-    // TODO first cut test, needs stucture
     public void testUnexpectedCallAlsoShowsExpectedCalls() {
-        Object obj1 = new Object();
-        Object obj2 = new Object();
+        String arg1 = "arg1";
+        String notArg1 = "not "+arg1;
+        Object arg2 = new Object();
+        Object notArg2 = new Object();
         Mock mock = new Mock(Types.WithTwoMethods.class, "MOCK_NAME");
         
         mock.method("twoArgsReturnsInt").with(ANYTHING, ANYTHING).willReturn(1)
             .expectOnce();
-        mock.method("twoArgsReturnsInt").with(eq("arg1"), same(obj1)).willReturn(1)
+        mock.method("twoArgsReturnsInt").with(eq(arg1), same(arg2)).willReturn(1)
             .expectOnce();
         
         try {
-            ((Types.WithTwoMethods)mock.proxy()).twoArgsReturnInt("not arg1", obj2);
-        } catch (Error error) {
-            assertEquals("Should be error message", 
-                    (Object)"MOCK_NAME: No match found\n" +
-                    "Invoked: twoArgsReturnInt(<not arg1>, <" + obj2 + ">)\n" +
-                    "in:\n" +
-                    "Method  = twoArgsReturnsInt, (<any value>, <any value>), expected once, returns <1>\n" +
-                    "Method  = twoArgsReturnsInt, (< = arg1>, <== <" + obj1 + ">>), expected once, returns <1>",
-                    error.getMessage().trim());
+            ((Types.WithTwoMethods)mock.proxy()).twoArgsReturnInt(notArg1, notArg2);
+        } catch (DynamicMockError error) {
+            String errorMessage = error.getMessage();
+            
+            assertStringContains( "should contain mock name", 
+                                  errorMessage, "MOCK_NAME" );
+            assertStringContains( "should report cause of error",
+                                   errorMessage, "No match found" );
+            assertSubstringOrder( "mock name should appear before cause of error",
+                                  errorMessage, "MOCK_NAME", "No match found" );
+            
+            assertStringContains( "should report method that caused error",
+                errorMessage, 
+                "twoArgsReturnInt(<"+notArg1+">, <" + notArg2 + ">)" );
+            
+            assertStringContains( "should report acceptable methods (first method)",
+                errorMessage, 
+                "twoArgsReturnsInt, (<any value>, <any value>), expected once, returns <1>" );
+            
+            assertStringContains( "should report acceptable methods (second method)",
+                errorMessage, 
+                "twoArgsReturnsInt, (<= "+arg1+">, <== <"+arg2+">>), expected once, returns <1>" );
+            
+            assertSubstringOrder( "should report acceptable methods in search order",
+                errorMessage,
+                "twoArgsReturnsInt, (<any value>, <any value>), expected once, returns <1>",
+                "twoArgsReturnsInt, (<= "+arg1+">, <== <"+arg2+">>), expected once, returns <1>" );
+            
             return;
         }
-
-        fail("Should have throw exception");
-   }
+        
+        fail("Should have thrown a DynamocMockError");
+    }
     
     public void testShowsNoExpectationsStringWhenNoExpectationsSet() {
         Mock mock = new Mock(Types.WithTwoMethods.class);
         try {
             ((Types.WithTwoMethods)mock.proxy()).twoArgsReturnInt("arg1", "arg2");
         } catch (Error error) {
-            assertEquals("Should be error message", 
-                (Object)"mockTypes$WithTwoMethods: No match found\n" +
-                "Invoked: twoArgsReturnInt(<arg1>, <arg2>)\n" +
-                "in:\n" +
-                "No expectations set",
-                (Object)error.getMessage().trim());
+            String errorMessage = error.getMessage();
+            
+            assertStringContains( "should report no expectations have been set",
+                errorMessage, "No expectations set" );
             return;
         }
 
         fail("Should have throw exception");
+    }
+    
+    public static void assertStringContains( String message, String string, String substring ) {
+        assertTrue( message + ": expected \"" + string + "\" to contain \"" + substring + "\"",
+                    string.indexOf(substring) >= 0 );
+    }
+    
+    public static void assertSubstringOrder( String message, String string, 
+											 String earlierSubstring, String laterSubstring )
+    {
+        assertStringContains( message, string, earlierSubstring );
+        assertStringContains( message, string, laterSubstring );
+        
+        int earlierIndex = string.indexOf(earlierSubstring);
+        int laterIndex = string.indexOf(laterSubstring);
+        
+        assertTrue( message+": expected \""+earlierSubstring+"\" "+
+                        "to appear before \"" + laterSubstring+"\" in \""+string+"\"",
+                    earlierIndex < laterIndex );
+                    
     }
 }
