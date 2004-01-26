950e5c43289f8f98eaa5e0b2a0ed9cbbfe1d1c95
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2004-01-26 15:47:09 +0000

    Cleaned up acceptance tests for error messages

--- src/acceptance-tests/atest/jmock/builder/ErrorMessagesAcceptanceTest.java
+++ src/acceptance-tests/atest/jmock/builder/ErrorMessagesAcceptanceTest.java
@@ -3,20 +3,21 @@
  */
 package atest.jmock.builder;
 
+import junit.framework.AssertionFailedError;
+
 import org.jmock.builder.Mock;
 import org.jmock.builder.MockObjectTestCase;
 import org.jmock.dynamic.DynamicMockError;
 
 public class ErrorMessagesAcceptanceTest extends MockObjectTestCase {
-
+    private static final String MOCK_NAME = "MOCK_NAME";
+    
     public void testUnexpectedCallAlsoShowsExpectedCalls() {
         String arg1 = "arg1";
         String notArg1 = "not "+arg1;
         Object arg2 = new Object();
         Object notArg2 = new Object();
-        Mock mock = new Mock(Types.WithTwoMethods.class, "MOCK_NAME");
-
-        String causeOfError = "no match found";
+        Mock mock = new Mock(Types.WithTwoMethods.class, MOCK_NAME);
 
         mock.method("twoArgsReturnsInt").with(ANYTHING, ANYTHING).willReturn(1)
             .expectOnce();
@@ -28,41 +29,45 @@ public class ErrorMessagesAcceptanceTest extends MockObjectTestCase {
         } catch (DynamicMockError error) {
             String errorMessage = error.getMessage();
             
+            String causeOfError = "no match found";
+            String expectedMethod1 = 
+                "twoArgsReturnsInt, (<any value>, <any value>), expected once, returns <1>";
+            String expectedMethod2 =  
+                "twoArgsReturnsInt, (<= "+arg1+">, <== <"+arg2+">>), expected once, returns <1>";
+            
             assertStringContains( "should contain mock name", 
-                                  errorMessage, "MOCK_NAME" );
+                                  errorMessage, MOCK_NAME );
+            
             assertStringContains( "should report cause of error",
                                    errorMessage, causeOfError );
+            
             assertSubstringOrder( "mock name should appear before cause of error",
-                                  errorMessage, "MOCK_NAME", causeOfError );
+                                  errorMessage, MOCK_NAME, causeOfError );
             
             assertStringContains( "should report method that caused error",
-                errorMessage, 
+                errorMessage,
                 "twoArgsReturnInt(<"+notArg1+">, <" + notArg2 + ">)" );
             
-            assertStringContains( "should report acceptable methods (first method)",
-                errorMessage, 
-                "twoArgsReturnsInt, (<any value>, <any value>), expected once, returns <1>" );
+            assertStringContains( "should report acceptable methods (#1)",
+                                  errorMessage, expectedMethod1 ); 
             
-            assertStringContains( "should report acceptable methods (second method)",
-                errorMessage, 
-                "twoArgsReturnsInt, (<= "+arg1+">, <== <"+arg2+">>), expected once, returns <1>" );
+            assertStringContains( "should report acceptable methods (#2)",
+                                  errorMessage, expectedMethod2 );
             
             assertSubstringOrder( "should report acceptable methods in search order",
-                errorMessage,
-                "twoArgsReturnsInt, (<any value>, <any value>), expected once, returns <1>",
-                "twoArgsReturnsInt, (<= "+arg1+">, <== <"+arg2+">>), expected once, returns <1>" );
+                errorMessage, expectedMethod1, expectedMethod2 );
             
             return;
         }
         
-        fail("Should have thrown a DynamicMockError");
+        fail("expected DynamicMockError");
     }
     
     public void testShowsNoExpectationsStringWhenNoExpectationsSet() {
         Mock mock = new Mock(Types.WithTwoMethods.class);
         try {
             ((Types.WithTwoMethods)mock.proxy()).twoArgsReturnInt("arg1", "arg2");
-        } catch (Error error) {
+        } catch (DynamicMockError error) {
             String errorMessage = error.getMessage();
             
             assertStringContains( "should report no expectations have been set",
@@ -70,7 +75,59 @@ public class ErrorMessagesAcceptanceTest extends MockObjectTestCase {
             return;
         }
 
-        fail("Should have throw exception");
+        fail("expected DynamicMockError");
+    }
+    
+    //TODO: what should be displayed when verify fails?
+    public void XtestShowPossibleMethodsWhenVerifyFails() {
+        Mock mock = new Mock(Types.WithTwoMethods.class,MOCK_NAME);
+        Object a2 = new Object();
+        Object b2 = new Object();
+        
+        mock.method("twoArgsReturnInt").with(eq("a1"),same(a2)).willReturn(1)
+            .expectOnce();
+        mock.method("twoArgsReturnInt").with(eq("b1"),same(b2)).willReturn(2)
+            .expectOnce();
+        mock.method("noArgsReturnsNothing").noParams().isVoid()
+            .expectAtLeastOnce();
+        
+        ((Types.WithTwoMethods)mock.proxy()).twoArgsReturnInt("b1",b2);
+        
+        try {
+            mock.verify();
+        }
+        catch( AssertionFailedError error ) {
+            String errorMessage = error.getMessage();
+            String causeOfError = "not all expected methods were invoked";
+            String expectedMethod1 =
+                "twoArgsReturnsInt, (<= a1>, <== <"+a2+">>), expected once, returns <1>";
+            String expectedMethod2 = 
+                "noArgsReturnsNothing, (no arguments), expected at least once, returns(<void>)";
+            String calledMethod =
+                "twoArgsReturnsInt, (<= b1>, <== <"+b2+">>), expected once and has been invoked, returns <2>";
+            
+            assertStringContains( "should contain mock name", 
+                                  errorMessage, MOCK_NAME );
+            
+            assertStringContains( "should report cause of error",
+                                   errorMessage, causeOfError );
+            
+            assertSubstringOrder( "mock name should appear before cause of error",
+                                  errorMessage, MOCK_NAME, causeOfError );
+            
+            assertStringContains( "should report uncalled methods (#1)",
+                errorMessage, expectedMethod1 );
+            
+            assertStringContains( "should report uncalled methods (#2)",
+                errorMessage, expectedMethod2 );
+            
+            assertStringContains( "should list include methods in error message",
+                errorMessage, calledMethod );
+            
+            return;
+        }
+        
+        fail("expected AssertionFailedError");
     }
     
     public static void assertStringContains( String message, String string, String substring ) {
