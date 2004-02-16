7a7b639150d302e233ea9d11bdd3d46e9b79c3b6
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2004-02-16 18:08:29 +0000

    [np] Fixed errors in CI scripts [np] Factored different tasks into separate CI scripts

--- damagecontrol.sh
+++ damagecontrol.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 # Damage control build script for jMock.
 
+
+
 sh continuous-integration/build-website.sh
+sh continuous-integration/build-javadocs.sh
+
+sh continuous-integration/deploy-website.sh
