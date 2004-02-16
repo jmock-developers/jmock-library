5a66f34176e8ab9f3a5e690f74d72f1745acaaf6
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2004-02-16 17:12:56 +0000

    [np] Factored out AbstractDynamicMock class

--- core/src/org/jmock/dynamic/CoreMock.java
+++ core/src/org/jmock/dynamic/CoreMock.java
@@ -5,23 +5,12 @@ import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 
-import junit.framework.AssertionFailedError;
-
-import org.jmock.Constraint;
-import org.jmock.constraint.IsAnything;
-import org.jmock.dynamic.matcher.ArgumentsMatcher;
-import org.jmock.dynamic.matcher.NoArgumentsMatcher;
-import org.jmock.dynamic.stub.CustomStub;
-import org.jmock.dynamic.stub.ReturnStub;
-
 
 public class CoreMock 
-	implements DynamicMock, InvocationHandler
+    extends AbstractDynamicMock
+	implements InvocationHandler
 {
-    private InvocationDispatcher invocationDispatcher;
-    private Class mockedType;
     private Object proxy;
-    private String name;
     
     public CoreMock( Class mockedType, String name ) {
         this( mockedType, name, new LIFOInvocationDispatcher() );
@@ -31,18 +20,10 @@ public class CoreMock
                      String name, 
                      InvocationDispatcher invocationDispatcher ) 
     {
-        this.mockedType = mockedType;
+        super( mockedType, name, invocationDispatcher );
         this.proxy = Proxy.newProxyInstance( getClass().getClassLoader(), 
                                              new Class[]{mockedType}, 
                                              this);
-        this.name = name;
-        this.invocationDispatcher = invocationDispatcher;
-        
-        setupDefaultBehaviour();
-    }
-    
-    public Class getMockedType() {
-        return mockedType;
     }
     
     public Object proxy() {
@@ -54,95 +35,5 @@ public class CoreMock
     {
         Invocation invocation = new Invocation(invokedProxy,method, args);
         return mockInvocation(invocation);
-    }
-    
-    private Object mockInvocation( Invocation invocation ) 
-        throws Throwable 
-    {
-        try {
-            return invocationDispatcher.dispatch(invocation);
-        }
-        catch (AssertionFailedError failure) {
-            DynamicMockError mockFailure = 
-            	new DynamicMockError(this, invocation, invocationDispatcher, failure.getMessage());
-            
-			mockFailure.fillInStackTrace();
-			throw mockFailure;
-        }
-    }
-
-    public void verify() {
-        try {
-            invocationDispatcher.verify();
-        } catch (AssertionFailedError ex) {
-            throw new AssertionFailedError(name + ": " + ex.getMessage());
-        }
-    }
-    
-    public String toString() {
-        return this.name;
-    }
-    
-    public String getMockName() {
-        return this.name;
-    }
-    
-	public void setDefaultStub(Stub newDefaultStub) {
-		invocationDispatcher.setDefaultStub(newDefaultStub);
-	}
-	
-    public void add(Invokable invokable) {
-        invocationDispatcher.add(invokable);
-    }
-    
-    public void reset() {
-        //TODO write tests for this
-        invocationDispatcher.clear();
-        setupDefaultBehaviour();
-    }
-    
-    public static String mockNameFromClass(Class c) {
-        return "mock" + DynamicUtil.classShortName(c);
-    }
-    
-    private void setupDefaultBehaviour() {
-        add( new SilentInvocationMocker( "toString", 
-            NoArgumentsMatcher.INSTANCE, 
-            new ReturnStub(name)));
-        add( new SilentInvocationMocker("equals", 
-            new ArgumentsMatcher(new Constraint[] {new IsAnything()}), 
-            new IsSameAsProxyStub()));
-        add( new SilentInvocationMocker("hashCode",
-            NoArgumentsMatcher.INSTANCE,
-            new HashCodeStub()));
-    }
-    
-    private static class SilentInvocationMocker extends InvocationMocker {
-        public SilentInvocationMocker(String methodName, InvocationMatcher arguments, Stub stub) {
-            super(methodName, arguments, stub);
-        }
-
-        public StringBuffer writeTo(StringBuffer buffer) {
-            return buffer;
-        }
-    }
-    
-    private class IsSameAsProxyStub extends CustomStub {
-        private IsSameAsProxyStub() {
-            super("returns whether equal to proxy");
-        }
-        
-        public Object invoke( Invocation invocation ) throws Throwable {
-            return new Boolean(invocation.getParameterValues().get(0) == proxy);
-        }
-    }
-    
-    private class HashCodeStub extends CustomStub {
-        private HashCodeStub() {
-            super("returns hashCode for proxy");
-        }
-        public Object invoke( Invocation invocation ) throws Throwable {
-            return new Integer(CoreMock.this.hashCode());
-        }
     }
 }
