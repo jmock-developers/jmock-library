9eb483fdfe2eac871600d4ce1be101845bb8a188
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2004-02-17 18:41:14 +0000

    [np] Make deployment optional (and on by default)

--- jmock/damagecontrol.sh
+++ jmock/damagecontrol.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 # Damage control build script for jMock.
 
+#export RELEASE_ID=1.0
+#export PRERELEASE_ID=1.0-RC1
+export SNAPSHOT_ID=$(date +%Y%m%d-%H%M%S)
+
 sh continuous-integration/build-website.sh
 sh continuous-integration/build-javadocs.sh
 
-sh continuous-integration/deploy-website.sh
+# deploy by default
+if ((${DEPLOY:-1})); then
+	sh continuous-integration/deploy-website.sh;
+fi
+
+echo done.
