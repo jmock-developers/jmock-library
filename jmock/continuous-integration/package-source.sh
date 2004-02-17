4af75d1162088d79bf74c46b41da37cd4075deac
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2004-02-17 19:41:36 +0000

    [np] CI task to create package source snapshots

---
+++ jmock/continuous-integration/package-source.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# Package a source snapshot
+
+ROOT=jmock-$SNAPSHOT_ID
+
+ln -s . $ROOT
+
+ALLSRC=$(echo $ROOT/{core,examples,extensions,lib,.classpath,.project,.cvsignore,BUILD.txt,LICENSE.txt})
+CALCULATOR_SRC=$ROOT/examples/calculator
+
+mkdir -p $PACKAGEDIR
+jar cMf $PACKAGEDIR/jmock-SNAPSHOT-src.jar $ALLSRC
+jar cMf $PACKAGEDIR/calculator-example-SNAPSHOT-src.jar $CALCULATOR_SRC
+
+rm $ROOT
--- jmock/damagecontrol.sh
+++ jmock/damagecontrol.sh
@@ -5,13 +5,13 @@
 #export PRERELEASE_ID=1.0-RC1
 export SNAPSHOT_ID=$(date +%Y%m%d-%H%M%S)
 
+export PACKAGEDIR=packages
 export WEBDIR=website/output
 export JAVADOCDIR=$WEBDIR/docs/javadoc
 
-
-
 sh continuous-integration/build-website.sh
 sh continuous-integration/build-javadocs.sh
+sh continuous-integration/package-source.sh
 
 # deploy by default
 if ((${DEPLOY:-1})); then
