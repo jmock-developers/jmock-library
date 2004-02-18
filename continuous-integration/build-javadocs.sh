e716bc783cfdc5f5daa272aff637f73b000e1884
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2004-02-18 18:50:59 +0000

    [np] Improved javadoc output

--- continuous-integration/build-javadocs.sh
+++ continuous-integration/build-javadocs.sh
@@ -6,11 +6,16 @@ SRCDIRS=core/src:extensions/cglib/src:extensions/dynamock/src
 LIBJARS=lib/*.jar
 
 $JAVA_HOME/bin/javadoc \
+	-quiet \
 	-windowtitle 'jMock API Documentation' \
 	-d $JAVADOCDIR \
 	-link http://www.junit.org/junit/javadoc/3.8.1 \
 	-link http://java.sun.com/j2se/1.4.2/docs/api \
 	-link http://cglib.sourceforge.net/apidocs \
 	-sourcepath $SRCDIRS \
-	-subpackages org.jmock
+	-subpackages org.jmock \
+	-group "User API" org.jmock.builder \
+	-group "Extension API" org.jmock:org.jmock.constraint:org.jmock.dynamic:org.jmock.dynamic.matcher:org.jmock.dynamic.stub:org.jmock.util \
+	-group "Optional Extensions" org.jmock.cglib:org.jmock.dynamock \
+	-group "Legacy API" org.jmock.expectation
 exit $?
