fd8a5877d176403844162d116245655031daa53e
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2004-02-26 21:10:32 +0000

    Added javadoc comments

--- core/src/org/jmock/core/SelfDescribing.java
+++ core/src/org/jmock/core/SelfDescribing.java
@@ -1,6 +1,16 @@
 package org.jmock.core;
 
 
+/**
+ * An object that can describe itself.
+ */
 public interface SelfDescribing {
+    /**
+     * Appends the description of this object to the buffer.
+     * 
+     * @param buffer
+     *     The buffer that the description is appended to.
+     * @return The buffer passed to the method.
+     */
     StringBuffer describeTo( StringBuffer buffer );
 }
