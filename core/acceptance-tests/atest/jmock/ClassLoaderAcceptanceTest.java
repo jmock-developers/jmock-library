b43e50149b0aac255e83c023d6a1acfd53d14cfc
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2004-03-11 16:40:50 +0000

    [np] Added acceptance test to show bug when Mock class and mocked interface are loaded from different class loaders [np] Fixed the bug

---
+++ core/acceptance-tests/atest/jmock/ClassLoaderAcceptanceTest.java
@@ -0,0 +1,36 @@
+package atest.jmock;
+
+import org.jmock.Mock;
+import org.jmock.MockObjectTestCase;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.Constants;
+
+
+public class ClassLoaderAcceptanceTest extends MockObjectTestCase {
+    
+    static class EmptyInterfaceCreator extends ClassLoader {
+        ClassLoader resourceLoader;
+        
+        protected Class findClass(String name) {
+            ClassWriter writer = new ClassWriter(true);
+            writer.visit(Constants.ACC_PUBLIC|Constants.ACC_INTERFACE,
+                         name.replace('.','/'),
+                         "java/lang/Object",
+                         null, /* interfaces */
+                         null /* source file */ );
+            
+            byte[] b = writer.toByteArray();
+            
+            return defineClass( name, b, 0, b.length );
+        }
+    }
+    
+    public void testMockingTypeFromOtherClassLoader() throws ClassNotFoundException {
+        ClassLoader interfaceClassLoader = new EmptyInterfaceCreator();
+        Class interfaceClass = interfaceClassLoader.loadClass("$UniqueTypeName$");
+        
+        Mock mock = new Mock(interfaceClass); // Should not throw an exception
+        
+        mock.verify(); // stop warning about unused variable
+    }
+}
