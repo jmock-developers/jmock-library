b5bbd7d7da340e15fe8533110dec49517d0adb8f
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2004-04-21 18:39:16 +0000

    ReturnStub reports type errors nicely

--- jmock/core/src/test/jmock/core/stub/ReturnStubTest.java
+++ jmock/core/src/test/jmock/core/stub/ReturnStubTest.java
@@ -2,27 +2,36 @@
 package test.jmock.core.stub;
 
 import junit.framework.TestCase;
+import junit.framework.AssertionFailedError;
 
 import org.jmock.core.Invocation;
 import org.jmock.core.stub.ReturnStub;
+import org.jmock.expectation.AssertMo;
 
 public class ReturnStubTest 
 	extends TestCase 
 {
 	static final String RESULT = "result";
-	
+
+	Object invokedObject;
+	Class invokedObjectClass;
     Invocation invocation;
     ReturnStub returnStub; 
     
     public void setUp() {
-	    invocation = new Invocation( 
-            "INVOKED-OBJECT", Void.class, 
-            "ignoredName", new Class[0], void.class, new Object[0] );
+	    invokedObject = "INVOKED-OBJECT";
+	    invokedObjectClass = Void.class;
+
 	    returnStub  = new ReturnStub(RESULT);
     }
     
     public void testReturnsValuePassedToConstructor() throws Throwable {
-        assertSame( "Should be the same result object", 
+	    invocation = new Invocation(
+	        invokedObject,
+            invokedObjectClass, "ignoredMethodNameName", new Class[0], RESULT.getClass(),
+            new Object[0] );
+
+        assertSame( "Should be the same result object",
         		    RESULT, returnStub.invoke(invocation) );
     }
     
@@ -38,4 +47,57 @@ public class ReturnStubTest
     	assertTrue( "contains 'returns' in description",
     				description.indexOf("returns") >= 0 );
     }
+	
+	public void testThrowsAssertionFailedErrorIfTriesToReturnValueOfIncompatibleType()
+		throws Throwable
+	{
+		invocation = new Invocation(
+		    invokedObject,
+	        invokedObjectClass, "ignoredMethodNameName", new Class[0], int.class,
+	        new Object[0] );
+
+	    try {
+		    returnStub.invoke(invocation);
+	    }
+		catch( AssertionFailedError ex ) {
+		    AssertMo.assertIncludes( "expected return type", invocation.getReturnType().toString(), ex.getMessage() );
+		    AssertMo.assertIncludes( "returned value type", RESULT.getClass().toString(), ex.getMessage() );
+		    return;
+	    }
+		fail("should have failed");
+	}
+
+	public void testCanReturnNullReference()
+		throws Throwable
+	{
+		invocation = new Invocation(
+		    invokedObject,
+	        invokedObjectClass, "ignoredMethodNameName", new Class[0], String.class,
+	        new Object[0] );
+
+		returnStub = new ReturnStub(null);
+
+		assertNull( "should return null", returnStub.invoke(invocation) );
+	}
+
+	public void testThrowsAssertionFailedErrorIfTriesToReturnNullFromMethodWithPrimitiveReturnType()
+		throws Throwable
+	{
+		invocation = new Invocation(
+		    invokedObject,
+	        invokedObjectClass, "ignoredMethodNameName", new Class[0], int.class,
+	        new Object[0] );
+
+		returnStub = new ReturnStub(null);
+
+		try {
+			returnStub.invoke(invocation);
+		}
+		catch( AssertionFailedError ex ) {
+			AssertMo.assertIncludes( "expected return type", invocation.getReturnType().toString(), ex.getMessage() );
+			AssertMo.assertIncludes( "null", String.valueOf((Object)null), ex.getMessage() );
+			return;
+		}
+		fail("should have failed");
+	}
 }
