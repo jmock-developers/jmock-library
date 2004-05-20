382078c25e38c9803abd1db42544bbd5ef554ab6
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2004-05-20 12:20:47 +0000

    Deployment script moved to damagecontrol.sh

--- jmock/continuous-integration/deploy-snapshots.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-#
-# Script to deploy packages to the jMock distribution site.
-
-SOURCE_DIST_DIR=$DISTROOT/distributions/
-
-ssh $DISTHOST mkdir -p $SOURCE_DIST_DIR
-scp $PACKAGEDIR/*-SNAPSHOT-src.jar $DISTHOST:$SOURCE_DIST_DIR
-exit $?
