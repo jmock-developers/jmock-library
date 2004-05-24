98fe13e550da356c907cf7dd2ea4ff8d0e917563
(no author) <(no author)@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2004-05-24 20:05:13 +0000

    This commit was manufactured by cvs2svn to create tag 'v1_0_0_RC1'.

--- jmock/examples/calculator/src/org/jmock/examples/calculator/Expression.java
+++
@@ -1,9 +0,0 @@
-/*  Copyright (c) 2000-2004 jMock.org
- */
-package org.jmock.examples.calculator;
-
-
-public interface Expression
-{
-    double evaluate( Environment environment ) throws CalculatorException;
-}
--- jmock/examples/publisher/src/org/jmock/examples/website/Subscriber.java
+++
@@ -1,9 +0,0 @@
-/*  Copyright (c) 2000-2004 jMock.org
- */
-package org.jmock.examples.website;
-
-
-interface Subscriber
-{
-    void receive( Message message );
-}
