c6fd2a7399daa0ec0cc654c95b81e3bdd0d89845
joe <joe@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2005-01-15 18:30:12 +0000

    BeetleJuice wrapper

---
+++ beetlejuice.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+# BeetleJuice wrapper for continuous integreation scripts. 
+# (BeetleJuice always uses /bin/sh, so we have to explicitly start /bin/bash)
+exec /bin/bash damagecontrol.sh $*
