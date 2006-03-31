eb0175f13cf477c449606555e10fff3298d89d96
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2006-03-31 19:36:41 +0000

    Clearing up the CI build scripts

--- beetlejuice.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# BeetleJuice wrapper for continuous integreation scripts. 
-# (BeetleJuice always uses /bin/sh, so we have to explicitly start /bin/bash)
-exec /bin/bash damagecontrol.sh $*
- 
