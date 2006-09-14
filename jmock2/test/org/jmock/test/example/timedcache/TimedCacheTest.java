547e5ffcffbad4508843f58c02d03e2ca003d12e
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2006-09-14 11:32:52 +0000

    TimedCache example now works

--- jmock2/test/org/jmock/test/example/timedcache/TimedCacheTest.java
+++ jmock2/test/org/jmock/test/example/timedcache/TimedCacheTest.java
@@ -2,39 +2,41 @@
  */
 package org.jmock.test.example.timedcache;
 
-import junit.framework.TestCase;
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.core.IsEqual;
-import org.jmock.InAnyOrder;
-import org.jmock.InThisOrder;
-import org.jmock.Mockery;
-import org.jmock.test.example.timedcache.Clock;
-import org.jmock.test.example.timedcache.ObjectLoader;
-import org.jmock.test.example.timedcache.ReloadPolicy;
-import org.jmock.test.example.timedcache.TimedCache;
-
+import java.util.Calendar;
 import java.util.Date;
 
+import junit.framework.TestCase;
 
-public class TimedCacheTest extends TestCase
-{
+import org.jmock.InAnyOrder;
+import org.jmock.Mockery;
+import org.jmock.integration.junit3.JUnit3ErrorTranslator;
 
+
+public class TimedCacheTest extends TestCase {
     final private Object KEY = "key";
     final private Object VALUE = "value";
     final private Object NEW_VALUE = "newValue";
 
-    private Mockery context = new Mockery();
-
+    private Mockery context = new Mockery() {{
+        setExpectationErrorTranslator(new JUnit3ErrorTranslator());
+    }};
+    
     private Clock mockClock = context.mock(Clock.class);
     private ObjectLoader mockLoader = context.mock(ObjectLoader.class);
     private ReloadPolicy mockReloadPolicy = context.mock(ReloadPolicy.class);
 
     private TimedCache cache = new TimedCache(mockLoader, mockClock, mockReloadPolicy);
 
-    private Date loadTime = new Date(1);
-    private Date fetchTime = new Date(2);
-    private Date reloadTime = new Date(3);
+    private Date loadTime = time(1);
+    private Date fetchTime = time(2);
 
+    private Date time(int i) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.clear();
+        calendar.set(Calendar.DAY_OF_YEAR, i);
+        return calendar.getTime();
+    }
+    
     public void testLoadsObjectThatIsNotCached() {
         final Object VALUE1 = "value1";
         final Object VALUE2 = "value2";
@@ -46,38 +48,49 @@ public class TimedCacheTest extends TestCase
             exactly(1).of (mockLoader).load("key2"); will(returnValue(VALUE2));
         }});
 
-        MatcherAssert.assertThat(cache.lookup("key1"), IsEqual.eq(VALUE1));
-        MatcherAssert.assertThat(cache.lookup("key2"), IsEqual.eq(VALUE2));
-
+        Object actualValue1 = cache.lookup("key1");
+        Object actualValue2 = cache.lookup("key2");
+        
         context.assertIsSatisfied();
+        assertEquals("lookup with key1", VALUE1, actualValue1);
+        assertEquals("lookup with key2", VALUE2, actualValue2);
     }
 
     public void testReturnsCachedObjectWithinTimeout() {
         context.expects(new InAnyOrder() {{
-            allow (mockClock).getCurrentTime(); will(returnValue(fetchTime));
+            exactly(1).of (mockClock).getCurrentTime(); will(returnValue(loadTime));
+            exactly(1).of (mockClock).getCurrentTime(); will(returnValue(fetchTime));
+            
             allow (mockReloadPolicy).shouldReload(loadTime, fetchTime); will(returnValue(false));
 
             exactly(1).of (mockLoader).load(KEY); will(returnValue(VALUE));
         }});
 
+        Object actualValueFromFirstLookup = cache.lookup(KEY);
+        Object actualValueFromSecondLookup = cache.lookup(KEY);
+        
         context.assertIsSatisfied();
-        assertSame("should be loaded object", VALUE, cache.lookup(KEY));
-        assertSame("should be cached object", VALUE, cache.lookup(KEY));
+        assertSame("should be loaded object", VALUE, actualValueFromFirstLookup);
+        assertSame("should be cached object", VALUE, actualValueFromSecondLookup);
     }
 
     public void testReloadsCachedObjectAfterTimeout() {
         context.expects(new InAnyOrder() {{
-            allow (mockClock).getCurrentTime(); will(returnValue(fetchTime));
+            exactly(1).of (mockClock).getCurrentTime(); will(returnValue(loadTime));
+            exactly(1).of (mockClock).getCurrentTime(); will(returnValue(fetchTime));
+            
             allow (mockReloadPolicy).shouldReload(loadTime, fetchTime); will(returnValue(true));
 
-            context.expects(new InThisOrder() {{
-                exactly(1).of (mockLoader).load(KEY); will(returnValue(VALUE));
-                exactly(1).of (mockLoader).load(KEY); will(returnValue(NEW_VALUE));
-            }});
+            exactly(1).of (mockLoader).load(KEY); will(returnValue(VALUE));
+            exactly(1).of (mockLoader).load(KEY); will(returnValue(NEW_VALUE));
         }});
 
+        Object actualValueFromFirstLookup = cache.lookup(KEY);
+        Object actualValueFromSecondLookup = cache.lookup(KEY);
+
         context.assertIsSatisfied();
-        assertSame("should be loaded object", VALUE, cache.lookup(KEY));
-        assertSame("should be reloaded object", NEW_VALUE, cache.lookup(KEY));
+        
+        assertSame("should be loaded object", VALUE, actualValueFromFirstLookup);
+        assertSame("should be reloaded object", NEW_VALUE, actualValueFromSecondLookup);
     }
 }
