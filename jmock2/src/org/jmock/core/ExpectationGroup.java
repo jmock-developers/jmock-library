85a54fd602f8fbacc3d87ae6b0877dd8d95565b5
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2006-09-21 14:55:03 +0000

    Merged interfaces

--- jmock2/src/org/jmock/core/ExpectationGroup.java
+++ jmock2/src/org/jmock/core/ExpectationGroup.java
@@ -1,9 +1,16 @@
 package org.jmock.core;
 
 /**
- * Dispatches invocations to multiple expectations
+ * Dispatches invocations to multiple expectations and defines ordering constraints
+ * over the expectations in the group.
  * 
  * @author npryce
  */
-public interface ExpectationGroup extends Expectation, ExpectationCollection {
+public interface ExpectationGroup extends Expectation {
+    /**
+     * Adds an expectation to the group.
+     * 
+     * @param expectation
+     */
+    void add(Expectation expectation);
 }
