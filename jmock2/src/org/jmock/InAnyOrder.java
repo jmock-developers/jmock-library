73a5f368110e8eedfd0692bcece33ddffd76e3db
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2006-09-22 17:20:16 +0000

    Ant build runs the test (but needs junit on the classpath)

--- jmock2/src/org/jmock/InAnyOrder.java
+++ jmock2/src/org/jmock/InAnyOrder.java
@@ -3,6 +3,9 @@ package org.jmock;
 import org.jmock.internal.ExpectationGroupBuilder;
 import org.jmock.lib.UnorderedExpectationGroup;
 
+/**
+ * A block of expectations that can occur in any order relative to one another.
+ */
 public class InAnyOrder extends ExpectationGroupBuilder {
     public InAnyOrder() {
         super(new UnorderedExpectationGroup());
--- jmock2/test/org/jmock/test/unit/integration/junit3/MockObjectTestCaseTests.java
+++ jmock2/test/org/jmock/test/unit/integration/junit3/MockObjectTestCaseTests.java
@@ -1,6 +1,8 @@
 package org.jmock.test.unit.integration.junit3;
 
-public class MockObjectTestCaseTests {
+import org.jmock.integration.junit3.MockObjectTestCase;
+
+public class MockObjectTestCaseTests extends MockObjectTestCase {
     public void testDoesNotNeedToHaveExpectationsSpecified() {
         // no expectations: the test should not fail
     }
