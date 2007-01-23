6917a470eaa5a37550d8b898fbb0120b0ed0a95c
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2007-01-23 16:38:52 +0000

    Deleted unnecessary inner class

--- test/org/jmock/test/acceptance/HamcrestTypeSafetyAcceptanceTest.java
+++ test/org/jmock/test/acceptance/HamcrestTypeSafetyAcceptanceTest.java
@@ -1,18 +1,16 @@
 package org.jmock.test.acceptance;
 
-import static org.hamcrest.text.StringStartsWith.startsWith;
 import static org.hamcrest.number.OrderingComparisons.greaterThan;
+import static org.hamcrest.text.StringStartsWith.startsWith;
 
 import java.lang.reflect.Method;
-import java.util.regex.Pattern;
 
 import junit.framework.TestCase;
 
-import org.hamcrest.Description;
 import org.hamcrest.Matcher;
-import org.hamcrest.TypeSafeMatcher;
 import org.jmock.InAnyOrder;
 import org.jmock.Mockery;
+import org.jmock.internal.MethodNameMatcher;
 
 public class HamcrestTypeSafetyAcceptanceTest extends TestCase {
     public interface MockedType {
@@ -25,31 +23,14 @@ public class HamcrestTypeSafetyAcceptanceTest extends TestCase {
     
     public void testMatchersCanCopeWithDifferentArgumentTypes() {
         context.expects(new InAnyOrder() {{
-            exactly(1).of (anything()).method(named("m")).with(startsWith("x"));
-            exactly(1).of (anything()).method(named("m")).with(greaterThan(0));
+            exactly(1).of (anything()).method(withName("m")).with(startsWith("x"));
+            exactly(1).of (anything()).method(withName("m")).with(greaterThan(0));
         }});
         
         mock.m(1); // should not throw ClassCastException
     }
     
-    Matcher<Method> named(String nameRegex) {
+    Matcher<Method> withName(String nameRegex) {
         return new MethodNameMatcher(nameRegex);
-    }
-    
-    private class MethodNameMatcher extends TypeSafeMatcher<Method> {
-        Pattern namePattern;
-        
-        public MethodNameMatcher(String nameRegex) {
-            namePattern = Pattern.compile(nameRegex);
-        }
-        
-        @Override
-        public boolean matchesSafely(Method method) {
-            return namePattern.matcher(method.getName()).matches();
-        }
-
-        public void describeTo(Description description) {
-            description.appendText("<"+namePattern+">");
-        }
     }
 }
