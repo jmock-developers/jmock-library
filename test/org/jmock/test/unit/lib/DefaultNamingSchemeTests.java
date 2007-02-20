acd53ae99c8be3f59c526784f78abd5672c333c7
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2007-02-20 16:19:24 +0000

    Nat: default naming scheme copes with class names that start with acromyms (HTTPClient, GPSReceiver) or are entire acronyms (URL, URI)

--- test/org/jmock/test/unit/lib/DefaultNamingSchemeTests.java
+++ test/org/jmock/test/unit/lib/DefaultNamingSchemeTests.java
@@ -1,5 +1,7 @@
 package org.jmock.test.unit.lib;
 
+import java.net.URL;
+
 import org.jmock.api.MockObjectNamingScheme;
 import org.jmock.lib.DefaultNamingScheme;
 import org.jmock.test.unit.support.DummyInterface;
@@ -7,10 +9,25 @@ import org.jmock.test.unit.support.DummyInterface;
 import junit.framework.TestCase;
 
 public class DefaultNamingSchemeTests extends TestCase {
+    MockObjectNamingScheme namingScheme = DefaultNamingScheme.INSTANCE;
+    
     public void testNamesMocksByLowerCasingFirstCharacterOfTypeName() {
-        MockObjectNamingScheme namingScheme = DefaultNamingScheme.INSTANCE;
-        
         assertEquals("runnable", namingScheme.defaultNameFor(Runnable.class));
         assertEquals("dummyInterface", namingScheme.defaultNameFor(DummyInterface.class));
+    }
+    
+    public interface GPSReceiver {}
+    public interface HTTPClient {};
+    public interface UDPDatagram {};
+    
+    public void testReturnsGoodNamesForClassesThatStartWithAcronyms() {
+        assertEquals("gpsReceiver", namingScheme.defaultNameFor(GPSReceiver.class));
+        assertEquals("httpClient", namingScheme.defaultNameFor(HTTPClient.class));
+        assertEquals("udpDatagram", namingScheme.defaultNameFor(UDPDatagram.class));
+        
+    }
+    
+    public void testReturnsGoodNamesForClassesThatAreEntirelyAcronyms() {
+        assertEquals("url", namingScheme.defaultNameFor(URL.class));
     }
 }
