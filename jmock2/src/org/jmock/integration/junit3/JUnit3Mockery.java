a4a907076af4b7c63ee8a82aa011d6e7cd5eab6a
(no author) <(no author)@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2007-04-10 15:35:42 +0000

    This commit was manufactured by cvs2svn to create tag 'V1_2_0'.

--- jmock2/src/org/jmock/integration/junit3/JUnit3Mockery.java
+++
@@ -1,9 +0,0 @@
-package org.jmock.integration.junit3;
-
-import org.jmock.Mockery;
-
-public class JUnit3Mockery extends Mockery {
-    public JUnit3Mockery() {
-        setExpectationErrorTranslator(JUnit3ErrorTranslator.INSTANCE);
-    }
-}
--- jmock2/src/org/jmock/internal/ExpectationBuilder.java
+++
@@ -1,7 +0,0 @@
-package org.jmock.internal;
-
-import org.jmock.api.Action;
-
-public interface ExpectationBuilder {
-    void buildExpectations(Action defaultAction, ExpectationCollector collector);
-}
