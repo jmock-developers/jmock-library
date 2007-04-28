30ed5e5335457028117f0af0fcc3af2e04be52b9
mauro <mauro@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2007-04-28 16:24:16 +0000

    Refactored deploy scripts to support any server via scp

--- deploy-file.sh
+++ deploy-file.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
-# Script to deploy a file to remove Maven repository
-# Requires an entry in the ~/.m2/settings.xml with the repository id and the webdav username/password
-# Copy settings.xml template to ~/.m2/settings.xml and replace with your username/password
+# Script to deploy a file to remote Maven repository
+# Requires an entry in the ~/.m2/settings.xml with the repository id
+# Copy settings.xml template to ~/.m2/settings.xml and replace with your username
 
 MVN_GOAL="mvn deploy:deploy-file"
-REPOSITORY_ID=codehaus.org
-URL=dav:https://dav.codehaus.org/repository/jmock
 
-if [ $# -lt 4 ]
+if [ $# -lt 6 ]
 then
-	echo "usage: deploy-file.sh <groupId> <artifactId> <version> <file>"
+	echo "usage: deploy-file.sh <repositoryId> <url> <groupId> <artifactId> <version> <file>"
 	exit -1
 fi
 
-GROUP_ID=$1
-ARTIFACT_ID=$2
-VERSION=$3
-FILE=$4
-$MVN_GOAL -DrepositoryId=$REPOSITORY_UD -Durl=$URL -DgroupId=$GROUP_ID -DartifactId=$ARTIFACT_ID -Dversion=$VERSION -Dfile=$FILE -Dpackaging=jar -DgeneratePom=true 
+REPOSITORY_ID=$1
+URL=$2
+GROUP_ID=$3
+ARTIFACT_ID=$4
+VERSION=$5
+FILE=$6
+$MVN_GOAL -DrepositoryId=$REPOSITORY_ID -Durl=$URL -DgroupId=$GROUP_ID -DartifactId=$ARTIFACT_ID -Dversion=$VERSION -Dfile=$FILE -Dpackaging=jar -DgeneratePom=true 
--- deploy-jars.sh
+++ deploy-jars.sh
@@ -1,43 +1,48 @@
 #!/bin/sh
-# Script to JMock artifacts to Codehaus Maven repository
+# Script to JMock artifacts to remote Maven repository which supports scp
 # System requirements:
 # 	- maven 2.0.x 
 #  	- wget 
 #	- unzip 
 
-if [ $# -lt 2 ]
+if [ $# -lt 4 ]
 then
-	echo "usage: deploy-jars.sh [jmock1|jmock2] <version>"
-	echo "eg: deploy-jars.sh jmock2 2.0.0"
+	echo "usage: deploy-jars.sh [jmock1|jmock2] <version> <repo-server> <repo-path>"
+	echo "eg: deploy-jars.sh jmock2 2.0.0 some.server.com path/to/repo"
 	exit -1
 fi
 
 TYPE=$1
 VERSION=$2
+REPO_SERVER=$3
+REPO_PATH=$4
+REPO_ID=$REPO_SERVER
+URL=scp://$REPO_ID/$REPO_PATH
 GROUP_ID=org.jmock
 JARS_ZIP=jmock-$VERSION-jars.zip
 DOWNLOAD_URL=http://www.jmock.org/dist/$JARS_ZIP
-wget $DOWNLOAD_URL
-DIR=jmock-$VERSION
-unzip $JARS_ZIP -d$DIR
-
+DOWNLOAD_DIR=target
+rm -r $DOWNLOAD_DIR
+wget $DOWNLOAD_URL -P $DOWNLOAD_DIR
+JARS_DIR=jmock-$VERSION
+unzip $DOWNLOAD_DIR/$JARS_ZIP -d$DOWNLOAD_DIR/$JARS_DIR
 
 if [ "$TYPE" = "jmock1" ]
 then	
 	ARTIFACT_LIST="jmock-core jmock-cglib"
-	FILE_ROOT=$DIR
+	FILE_DIR=$DOWNLOAD_DIR/$JARS_DIR
 fi
 if [ "$TYPE" = "jmock2" ]
 then
 	ARTIFACT_LIST="jmock jmock-junit3 jmock-junit4"
-	FILE_ROOT=$DIR/$DIR
+	FILE_DIR=$DOWNLOAD_DIR/$JARS_DIR/$JARS_DIR
 fi
 
 for ARTIFACT_ID in $ARTIFACT_LIST
 do
-	FILE=$FILE_ROOT/$ARTIFACT_ID-$VERSION.jar
+	FILE=$FILE_DIR/$ARTIFACT_ID-$VERSION.jar
 	echo "Deploying: $FILE"
-	#./deploy-file.sh $GROUP_ID $ARTIFACT_ID $VERSION $FILE
+	./deploy-file.sh $REPO_ID $URL $GROUP_ID $ARTIFACT_ID $VERSION $FILE
 done
 
 
