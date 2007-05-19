854eecf3e83628a92ee273b0f89012beae54101a
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2007-05-19 09:09:10 +0000

    More tests (and refactored tests) around class-loading

--- jmock2/test/org/jmock/test/acceptance/ClassLoaderAcceptanceTests.java
+++ jmock2/test/org/jmock/test/acceptance/ClassLoaderAcceptanceTests.java
@@ -2,53 +2,36 @@
  */
 package org.jmock.test.acceptance;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
 import junit.framework.TestCase;
-import net.sf.cglib.asm.ClassWriter;
-import net.sf.cglib.asm.Constants;
 
 import org.jmock.Mockery;
 import org.jmock.lib.legacy.ClassImposteriser;
-import org.jmock.test.unit.support.MethodFactory;
 
 
-public class ClassLoaderAcceptanceTests extends TestCase
-{
+public class ClassLoaderAcceptanceTests extends TestCase {
     Mockery mockery = new Mockery();
+    ClassLoader classLoader;
     
-    public void testMockingTypeFromOtherClassLoaderWithDefaultImposteriser() throws ClassNotFoundException {
-        Class<?> typeToMock = loadUniqueClass();
-        mockery.mock(typeToMock);
+    @Override
+    public void setUp() throws MalformedURLException {
+        classLoader = new URLClassLoader(new URL[]{new URL("file:build/testdata/unsigned.jar")}, null);
     }
     
-    public void testMockingTypeFromOtherClassLoaderWithClassImposteriser() throws ClassNotFoundException {
+    public void testMockingInterfaceFromOtherClassLoaderWithDefaultImposteriser() throws ClassNotFoundException {
+        mockery.mock(classLoader.loadClass("InterfaceFromOtherClassLoader"));
+    }
+    
+    public void testMockingInterfaceFromOtherClassLoaderWithClassImposteriser() throws ClassNotFoundException {
         mockery.setImposteriser(ClassImposteriser.INSTANCE);
-        mockery.mock(loadUniqueClass());
+        mockery.mock(classLoader.loadClass("InterfaceFromOtherClassLoader"));
     }
     
-    private Class<?> loadUniqueClass() throws ClassNotFoundException {
-        ClassLoader loader = new OrphanedFakeClassLoader();
-        return loader.loadClass("$UniqueTypeName_"+System.currentTimeMillis()+"$");
-    }
-
-    static class OrphanedFakeClassLoader extends ClassLoader {
-        public OrphanedFakeClassLoader() {
-            super(null);
-        }
-        
-        @Override
-        protected Class<?> findClass(String name) {
-            ClassWriter writer = new ClassWriter(true);
-            
-            writer.visit(MethodFactory.CLASS_FORMAT_VERSION, 
-                         Constants.ACC_PUBLIC | Constants.ACC_INTERFACE,
-                         name.replace('.', '/'),
-                         "java/lang/Object",
-                         null, /* interfaces */
-                         null /* source file */);
-
-            byte[] b = writer.toByteArray();
-            
-            return defineClass(name, b, 0, b.length);
-        }
+    public void testMockingClassFromOtherClassLoaderWithClassImposteriser() throws ClassNotFoundException {
+        mockery.setImposteriser(ClassImposteriser.INSTANCE);
+        mockery.mock(classLoader.loadClass("ClassFromOtherClassLoader"));
     }
 }
