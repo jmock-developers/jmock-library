ec99f2c0b6f8a6addda530e17412818395cc493a
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2007-07-23 13:28:58 +0000

    Error checking in tag script

--- jmock2/tag.sh
+++ jmock2/tag.sh
@@ -3,5 +3,14 @@ VERSION=${1:?no version number given}
 TRUNK=`svn info | grep URL: | cut -c 6-`
 ROOT=`svn info | grep 'Repository Root:' | cut -c 18-`
 
-svn copy $TRUNK $ROOT/tags/$VERSION
+CHANGES=`svn status`
+
+if [ ! -z "$CHANGES" ]; then
+	echo "Uncommitted changes:"
+	svn status
+	echo "Will not tag head revision as $VERSION until changes are committed"
+	exit 1
+fi
+
+echo svn copy $TRUNK $ROOT/tags/$VERSION
 
