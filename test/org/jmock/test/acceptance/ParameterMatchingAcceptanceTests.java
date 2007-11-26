f4f239204bafeb61eb21aa96ff88895270ff12c8
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2007-11-26 05:50:15 +0000

    Investigating issue JMOCK-160

--- test/org/jmock/test/acceptance/ParameterMatchingAcceptanceTests.java
+++ test/org/jmock/test/acceptance/ParameterMatchingAcceptanceTests.java
@@ -8,6 +8,7 @@ import org.jmock.Mockery;
 public class ParameterMatchingAcceptanceTests extends TestCase {
     public interface AnInterface {
         void doSomethingWith(String s);
+        void doSomethingWithBoth(String s1, String s2);
     }
     
     Mockery context = new Mockery();
@@ -33,6 +34,29 @@ public class ParameterMatchingAcceptanceTests extends TestCase {
         
         mock.doSomethingWith("hello");
         mock.doSomethingWith("goodbye");
+        
+        context.assertIsSatisfied();
+    }
+    
+    public void testAllOrNoneOfTheParametersMustBeSpecifiedByMatchers() {
+        try {
+            context.checking(new Expectations() {{
+                exactly(1).of (mock).doSomethingWithBoth(with(equal("a-matcher")), "not-a-matcher");
+            }});
+        }
+        catch (IllegalArgumentException expected) {
+        }
+    }
+    
+    // Test to show that issue JMOCK-160 is spurious
+    public void testNotAllExpectationsOfSameMockMustUseMatchers() {
+        context.checking(new Expectations() {{
+            exactly(1).of (mock).doSomethingWithBoth(with(equal("x")), with(equal("y")));
+            exactly(1).of (mock).doSomethingWith("z");
+        }});
+        
+        mock.doSomethingWithBoth("x", "y");
+        mock.doSomethingWith("z");
         
         context.assertIsSatisfied();
     }
