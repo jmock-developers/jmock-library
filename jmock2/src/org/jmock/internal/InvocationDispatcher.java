93566bdc0a3b2b131570ab88e5587de131e81b21
smgf <smgf@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2008-09-21 15:04:41 +0000

    work in progress on hamcrest spike

--- jmock2/src/org/jmock/internal/InvocationDispatcher.java
+++ jmock2/src/org/jmock/internal/InvocationDispatcher.java
@@ -1,15 +1,17 @@
 package org.jmock.internal;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.hamcrest.Description;
+import org.hamcrest.Mismatchable;
 import org.hamcrest.SelfDescribing;
 import org.jmock.api.Expectation;
 import org.jmock.api.ExpectationError;
 import org.jmock.api.Invocation;
 
-public class InvocationDispatcher implements ExpectationCollector, SelfDescribing {
+public class InvocationDispatcher implements ExpectationCollector, Mismatchable {
 	private List<Expectation> expectations = new ArrayList<Expectation>();
 	private List<StateMachine> stateMachines = new ArrayList<StateMachine>();
     
@@ -37,6 +39,42 @@ public class InvocationDispatcher implements ExpectationCollector, SelfDescribin
         }
     }
     
+    public void describeMismatch(Object invocation, Description description) {
+        if (expectations.isEmpty()) {
+            description.appendText("no expectations specified: did you...\n"+
+                                   " - forget to start an expectation with a cardinality clause?\n" +
+                                   " - call a mocked method to specify the parameter of an expectation?");
+        }
+        else {
+            description.appendList("expectations:\n  ", "\n  ", "", iterable(invocation, expectations));
+            if (!stateMachines.isEmpty()) {
+                description.appendList("\nstates:\n  ", "\n  ", "", stateMachines);
+            }
+        }
+        
+    }
+
+    private Iterable<SelfDescribing> iterable(final Object invocation, List<Expectation> expectations) {
+        final Iterator<Expectation> iterator = expectations.iterator();
+        return new Iterable<SelfDescribing>() {
+            public Iterator<SelfDescribing> iterator() {
+                return new Iterator<SelfDescribing>() {
+                    public boolean hasNext() { return iterator.hasNext(); }
+                    public SelfDescribing next(){
+                        return new SelfDescribing() {
+                            public void describeTo(Description description) {
+                                iterator.next().describeMismatch(invocation, description);
+                            }                            
+                        };
+                    }
+                    public void remove() { iterator.remove(); }
+                };
+            }
+            
+        };
+    }
+
+
     public boolean isSatisfied() {
 		for (Expectation expectation : expectations) {
 		    if (! expectation.isSatisfied()) {
@@ -45,14 +83,15 @@ public class InvocationDispatcher implements ExpectationCollector, SelfDescribin
         }
         return true;
 	}
-	
-	public Object dispatch(Invocation invocation) throws Throwable {
+    
+    public SuccessfulDispatch dispatch(Invocation invocation) throws Throwable {
 		for (Expectation expectation : expectations) {
 		    if (expectation.matches(invocation)) {
-		        return expectation.invoke(invocation);
+		        return new SuccessfulDispatch(invocation, expectation, expectation.invoke(invocation));
             }
         }
         
         throw new ExpectationError("unexpected invocation", invocation);
 	}
+
 }
---
+++ jmock2/src/org/jmock/internal/matcher/AllParametersMatcher.java
@@ -0,0 +1,64 @@
+package org.jmock.internal.matcher;
+
+import java.util.List;
+
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.collection.IsArray;
+import org.hamcrest.core.IsEqual;
+import org.jmock.internal.ParametersMatcher;
+
+public class AllParametersMatcher extends IsArray<Object> implements ParametersMatcher {
+    public AllParametersMatcher(Object[] expectedValues) {
+        super(equalMatchersFor(expectedValues));
+    }
+    
+    @SuppressWarnings("unchecked")
+    private static Matcher<Object>[] equalMatchersFor(Object[] expectedValues) {
+        Matcher<Object>[] matchers = new Matcher[expectedValues.length];
+        for (int i = 0; i < expectedValues.length; i++) {
+            matchers[i] = new IsEqual<Object>(expectedValues[i]);
+        }
+        return matchers;
+    }
+    
+    @Override
+    protected boolean matchesValuesSafely(Object[] values, Matcher<Object>[] matchers, Description description) {
+        boolean result = true;
+        description.appendText("      received values: (");
+        for (int i = 0; i < values.length; i++) {
+            final Matcher<Object> matcher = matchers[i];
+            
+            if (! matcher.matches(values[i])) {
+              description.appendText("mismatch<");
+              result = false;
+            } else {
+              description.appendText("match<");
+            }
+            matcher.describeMismatch(values[i], description);
+            description.appendText(">");
+            if (i < values.length - 1) {
+                description.appendText(", ");
+            } else {
+                description.appendText(")");
+            }
+        }
+        
+        return result;
+      }
+
+    @SuppressWarnings("unchecked")
+    public AllParametersMatcher(List<Matcher<?>> parameterMatchers) {
+        super(parameterMatchers.toArray(new Matcher[0]));
+    }
+
+    @Override
+    protected String descriptionStart() {
+        return "(";
+    }
+    
+    @Override
+    protected String descriptionEnd() {
+        return ")";
+    }
+}
