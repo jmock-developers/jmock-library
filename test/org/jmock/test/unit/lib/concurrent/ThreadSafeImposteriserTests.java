bf0d493a0a33cf642623021a1ec8912c5ea11fd3
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2009-03-01 21:01:31 +0000

    Tests for ThreadSafeImposteriser

--- test/org/jmock/test/unit/lib/concurrent/ThreadSafeImposteriserTests.java
+++ test/org/jmock/test/unit/lib/concurrent/ThreadSafeImposteriserTests.java
@@ -1,7 +1,12 @@
 package org.jmock.test.unit.lib.concurrent;
 
+import static org.junit.Assert.fail;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
 import org.jmock.Expectations;
 import org.jmock.Mockery;
+import org.jmock.States;
 import org.jmock.integration.junit4.JUnit4Mockery;
 import org.jmock.lib.JavaReflectionImposteriser;
 import org.jmock.lib.concurrent.Blitzer;
@@ -10,25 +15,76 @@ import org.junit.After;
 import org.junit.Test;
 
 public class ThreadSafeImposteriserTests {
+    public interface Events {
+        void action();
+        void finished();
+    }
+    
+    ThreadSafeImposteriser imposteriser = new ThreadSafeImposteriser(new JavaReflectionImposteriser());
+    
     Mockery mockery = new JUnit4Mockery() {{
-        setImposteriser(new ThreadSafeImposteriser(new JavaReflectionImposteriser()));
+        setImposteriser(imposteriser);
     }};
     
     Blitzer blitzer = new Blitzer(4, 1000);
     
-    Runnable mockObject = mockery.mock(Runnable.class, "mockObject");
+    Events mockObject = mockery.mock(Events.class, "mockObject");
     
     @Test(timeout=250)
     public void allowsMultipleThreadsToCallMockObjects() throws InterruptedException {
         mockery.checking(new Expectations() {{
-            exactly(blitzer.totalActionCount()).of(mockObject).run();
+            exactly(blitzer.totalActionCount()).of(mockObject).action();
         }});
         
-        blitzer.blitz(mockObject);
+        blitzer.blitz(new Runnable() {
+            public void run() {
+                mockObject.action();
+            }
+        });
         
         mockery.assertIsSatisfied();
     }
     
+    @Test(timeout=250)
+    public void canWaitForAStateMachineToEnterAGivenState() throws InterruptedException {
+        final AtomicInteger counter = new AtomicInteger(blitzer.totalActionCount());
+        
+        final States threads = mockery.states("threads");
+        
+        mockery.checking(new Expectations() {{
+            exactly(blitzer.totalActionCount()).of(mockObject).action();
+                when(threads.isNot("finished"));
+                
+            oneOf(mockObject).finished();
+                then(threads.is("finished"));
+        }});
+        
+        blitzer.blitz(new Runnable() {
+            public void run() {
+                mockObject.action();
+                if (counter.decrementAndGet() == 0) {
+                    mockObject.finished();
+                }
+            }
+        });
+        
+        imposteriser.waitUntil(threads.is("finished"), 100);
+    }
+    
+    @Test(timeout=250)
+    public void failsTheTestIfStateMachineDoesNotEnterExpectedStateWithinTimeout() throws InterruptedException {
+        States threads = mockery.states("threads");
+        
+        try {
+            imposteriser.waitUntil(threads.is("finished"), 100);
+        }
+        catch (AssertionError e) {
+            return;
+        }
+        
+        fail("should have thrown AssertionError");
+    }
+
     @After
     public void cleanUp() {
         blitzer.shutdown();
