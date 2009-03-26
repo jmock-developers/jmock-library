8baa09ad742bac3f55fcfbc7929e85e9df05cf7a
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2009-03-26 22:38:37 +0000

    JMOCK-219: fail test if class contains more than one Mockery

--- jmock2/test/org/jmock/test/acceptance/junit4/JUnit4TestRunnerTests.java
+++ jmock2/test/org/jmock/test/acceptance/junit4/JUnit4TestRunnerTests.java
@@ -3,6 +3,8 @@ package org.jmock.test.acceptance.junit4;
 import junit.framework.TestCase;
 
 import org.jmock.test.acceptance.junit4.testdata.DerivedJUnit4TestThatDoesNotSatisfyExpectations;
+import org.jmock.test.acceptance.junit4.testdata.JUnit4TestThatCreatesNoMockery;
+import org.jmock.test.acceptance.junit4.testdata.JUnit4TestThatCreatesTwoMockeries;
 import org.jmock.test.acceptance.junit4.testdata.JUnit4TestThatDoesNotCreateAMockery;
 import org.jmock.test.acceptance.junit4.testdata.JUnit4TestThatDoesNotSatisfyExpectations;
 import org.jmock.test.acceptance.junit4.testdata.JUnit4TestThatDoesSatisfyExpectations;
@@ -46,6 +48,18 @@ public class JUnit4TestRunnerTests extends TestCase {
         runTest(JUnit4TestThatThrowsExpectedException.class);
         assertTestFailedWith(AssertionError.class);
     }
+
+    // See issue JMOCK-219
+    public void testTheJUnit4TestRunnerReportsIfNoMockeryIsFound() {
+        runTest(JUnit4TestThatCreatesNoMockery.class);
+        assertTestFailedWithInitializationError();
+    }
+
+    // See issue JMOCK-219
+    public void testTheJUnit4TestRunnerReportsIfMoreThanOneMockeryIsFound() {
+        runTest(JUnit4TestThatCreatesTwoMockeries.class);
+        assertTestFailedWithInitializationError();
+    }
     
     public void testDetectsNonPublicBeforeMethodsCorrectly() {
         runTest(JUnit4TestWithNonPublicBeforeMethod.class);
@@ -59,7 +73,13 @@ public class JUnit4TestRunnerTests extends TestCase {
         assertTrue("should have failed with " + exceptionType.getName() + " but threw " + listener.failure.getException(), 
                    exceptionType.isInstance(listener.failure.getException()));
     }
-    
+
+    private void assertTestFailedWithInitializationError() {
+        assertNotNull("test should have failed", listener.failure);
+        assertTrue("should have failed with initialization error, but failure was " + listener.failure.toString(),
+                   listener.failure.getDescription().toString().contains("initializationError"));
+    }
+
     private void runTest(Class<?> testClass) {
         Runner runner = Request.aClass(testClass).getRunner();
         RunNotifier notifier = new RunNotifier();
--- jmock2/test/org/jmock/test/acceptance/junit4/testdata/JUnit4TestThatCreatesNoMockery.java
+++ jmock2/test/org/jmock/test/acceptance/junit4/testdata/JUnit4TestThatCreatesNoMockery.java
@@ -1,14 +1,11 @@
 package org.jmock.test.acceptance.junit4.testdata;
 
-import org.jmock.Mockery;
 import org.jmock.integration.junit4.JMock;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(JMock.class)
-public class JUnit4TestThatDoesNotCreateAMockery {
-    Mockery context = null;
-    
+public class JUnit4TestThatCreatesNoMockery {
     @Test
     public void happy() {
         // a-ok!
