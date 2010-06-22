7eb353fd37d69eb11898ab08759f4b55db74cc5e
npryce <npryce@8d24703b-d631-0410-a8e0-a14f6adf8faf>
2010-06-22 22:17:05 +0000

    nat - another bit of example code

---
+++ jmock2/example/org/jmock/example/gettingstarted/GettingStartedJUnit4RuleMockomatic.java
@@ -0,0 +1,32 @@
+package org.jmock.example.gettingstarted;
+
+import org.jmock.Expectations;
+import org.jmock.auto.Mock;
+import org.jmock.integration.junit4.JUnitRuleMockery;
+import org.junit.Rule;
+import org.junit.Test;
+
+public class GettingStartedJUnit4RuleMockomatic {
+    @Rule
+    public JUnitRuleMockery context = new JUnitRuleMockery();
+
+    @Mock Subscriber subscriber;
+
+    @Test
+    public void oneSubscriberReceivesAMessage() {
+        // set up
+        
+        Publisher publisher = new Publisher();
+        publisher.add(subscriber);
+
+        final String message = "message";
+
+        // expectations
+        context.checking(new Expectations() {{
+            oneOf(subscriber).receive(message);
+        }});
+
+        // execute
+        publisher.publish(message);
+    }
+}
