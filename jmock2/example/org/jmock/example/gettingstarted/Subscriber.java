db0b623291b3a8a842932d8b52b81b8005f67c84
Nat Pryce <github@natpryce.com>
2010-10-05 18:00:06 +0100

    removed jmock source from packaging branch

--- jmock2/example/org/jmock/example/gettingstarted/Subscriber.java
+++
@@ -1,5 +0,0 @@
-package org.jmock.example.gettingstarted;
-
-public interface Subscriber {
-    void receive(String message);
-}
--- jmock2/example/org/jmock/example/sniper/AuctionListener.java
+++
@@ -1,5 +0,0 @@
-package org.jmock.example.sniper;
-
-public interface AuctionListener {
-    void bidAccepted(Auction item, Money amount);
-}
