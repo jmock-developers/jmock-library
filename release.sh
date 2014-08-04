c4da5eaf0b08ffa4765c277cfa58ca2cb268c867
Nat Pryce <sw@natpryce.com>
2014-08-04 23:00:01 +0100

    automatic release script now compatible with github repo and hosting

--- release.sh
+++ release.sh
@@ -1,61 +1,63 @@
 #!/bin/bash
 # Release tool for jMock 2
+set -ex
 
 export VERSION=${1:?No version number given}
 export TAG=$VERSION
 
-# Configure ssh to use the appropriate user when logging into Codehaus
-REPOSITORY=https://svn.codehaus.org/jmock
+WORKING_DIR=$PWD/build/release/jmock-$VERSION
+WEBSITE_DIR=$PWD/build/release/jmock-website
 
-WORKING_DIR=build/release
-EXPORT_SUBDIR=jmock-$VERSION
-WEBSITE_SUBDIR=jmock-website
+JAVADOC_SUBDIR=javadoc/jmock-$VERSION
 
-REMOTE=${REMOTE:-jmock@www.jmock.org:/home/jmock}
-DIST=${DIST:-$REMOTE/public_dist}
-JAVADOC=${JAVADOC:-$REMOTE/public_javadoc}
-
-
-function export_release() {
-    svn export $REPOSITORY/tags/$VERSION $EXPORT_SUBDIR
-    if [ $? -ne 0 ]; then
-        exit 1
-    fi
-}
-
-function build_release() {
-    CLASSPATH=lib/junit-3.8.1.jar ant -Dversion=$VERSION
-    if [ $? -ne 0 ]; then
-        exit 1
-    fi
-}
-
-function publish_release() {
-    scp build/jmock-$VERSION-*.zip $DIST
-    if [ $? -ne 0 ]; then
-        exit 1
-    fi	
-}
-
-function publish_javadoc() {
-    scp -r build/jmock-$VERSION/doc/ $JAVADOC/$VERSION
-}
-
-function checkout_website() {
-    svn co $REPOSITORY/website $WEBSITE_SUBDIR
-    if [ $? -ne 0 ]; then
-        exit 1
-    fi
-}
 
 echo "Publishing release of jMock $VERSION to $DIST"
-rm -rf $WORKING_DIR
-mkdir -p $WORKING_DIR
-cd $WORKING_DIR
-export_release
-cd $EXPORT_SUBDIR
-build_release
-publish_release
-publish_javadoc
 
+ant clean
+
+rm -rf $WORKING_DIR
+git clone . $WORKING_DIR
+
+(
+    cd $WORKING_DIR
+    git checkout $TAG
+    CLASSPATH=lib/junit-3.8.1.jar ant -Dversion=$VERSION
+)
+
+rm -rf $WEBSITE_DIR
+git clone git@github.com:jmock-developers/jmock-website.git $WEBSITE_DIR
+
+(
+    cd $WEBSITE_DIR    
+    ./version.py data/versions.xml $VERSION
+    make all
+    git ci -a -m "publishing $VERSION"
+)
+
+(
+    cd $WORKING_DIR
+    git checkout gh-pages
+    cp -R $WEBSITE_DIR/skinned/* .
+    cp $WORKING_DIR/build/jmock-$VERSION-*.zip downloads/
+    rm -rf javadoc/jmock-$VERSION
+    cp -R $WORKING_DIR/build/jmock-$VERSION/doc/ javadoc/jmock-$VERSION
+    
+    git add downloads/ javadoc/
+    git ci -a -m "publishing $VERSION"
+)
+
+(
+    cd $WEBSITE_DIR
+    git push
+)
+
+(
+    cd $WORKING_DIR
+    git push origin gh-pages
+    git checkout jmock2
+)
+
+git co gh-pages
+git push
+git co jmock2
 
