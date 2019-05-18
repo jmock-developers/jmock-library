b63ad56cee68db4074047e9a019733a2bd5d2059
oliverbye <oli.github@mail-filter.com>
2019-05-18 10:42:56 +0100

    Move acceptance tests which are supposed to fail.

--- jmock-junit5/src/test/java/org/jmock/junit5/testdata/jmock/acceptance/JUnit5TestThatCreatesTwoMockeries.java
+++ jmock-junit5/src/test/java/org/jmock/junit5/testdata/jmock/acceptance/JUnit5TestThatCreatesTwoMockeries.java
@@ -1,4 +1,4 @@
-package org.jmock.junit5.testdata;
+package org.jmock.junit5.testdata.jmock.acceptance;
 
 import org.jmock.junit5.JUnit5Mockery;
 import org.junit.jupiter.api.Test;
--- jmock-junit5/src/test/java/org/jmock/junit5/testdata/jmock/acceptance/JUnit5TestThatDoesNotSatisfyExpectations.java
+++ jmock-junit5/src/test/java/org/jmock/junit5/testdata/jmock/acceptance/JUnit5TestThatDoesNotSatisfyExpectations.java
@@ -1,4 +1,4 @@
-package org.jmock.junit5.testdata;
+package org.jmock.junit5.testdata.jmock.acceptance;
 
 import org.jmock.Expectations;
 import org.jmock.auto.Mock;
